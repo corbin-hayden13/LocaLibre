f21e239b7517fd614108bf529526edbcca480147
hcorbin-ps1 <hcorbin1.polarionsystems@gmail.com>
2025-04-13 18:39:15 -0500

    There's a heading for the web app...

---
+++ frontend/src/components/WebAppHeading.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ELEM_HEADING, TEXT_BOLD } from "../common-themes";
+
+export function WebAppHeading() {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                padding: 0,
+                background: ELEM_HEADING,
+                minHeight: "40px",
+                maxHeight: "50px",
+                maxWidth: "100%",
+                boxSizing: "border-box",
+                flex: "1 1 auto",
+            }}
+        >
+            <h2 style={{ color: TEXT_BOLD, marginLeft: "20px" }} >LocaLibre</h2>
+        </div>
+    );
+}
