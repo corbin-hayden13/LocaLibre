d8366df95415aa09daf90a659740921238c6bc28
hcorbin-ps1 <hcorbin1.polarionsystems@gmail.com>
2025-04-29 10:44:08 -0500

    Added the "Add Game" modal and some input elements

--- frontend/src/components/AddGameModal.tsx
+++ frontend/src/components/AddGameModal.tsx
@@ -1,5 +1,6 @@
-import { Dialog, TextInputField } from "evergreen-ui";
+import { Button, Dialog, TextInputField } from "evergreen-ui";
 import { ChangeEvent } from "react";
+import { ELEM_BACKGROUND, ELEM_HEADING, TEXT_BASE, TEXT_BOLD } from "../common-themes";
 
 const DISPLAY_NAME: string = "displayName";
 const GAME_PATH: string = "gamePath";
@@ -39,39 +40,62 @@ export default function AddGameModal({isShown, onClose}: PropsWrapper) {
     return (
         <Dialog
             isShown={isShown}
-            title="Add A New Game"
+            title={<span style={{ color: TEXT_BOLD, fontWeight: "bold" }} >Add A New Game</span>}
             confirmLabel="Add Game"
             onCloseComplete={onClose}
+            containerProps={{
+                style: { background: ELEM_BACKGROUND, color: TEXT_BASE, border:`2px solid ${TEXT_BOLD}` }
+            }}
+            contentContainerProps={{
+                style: { color: TEXT_BASE }
+            }}
+            footer={({ close }) => (
+                <div style={{ display: "flex", justifyContent: "flex-end", gap: 10 }} >
+                    <Button
+                        style={{
+                            background: ELEM_HEADING,
+                            color: TEXT_BASE
+                        }}
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        style={{
+                            background: TEXT_BOLD
+                        }}
+                    >
+                        Add Game
+                    </Button>
+                </div>
+            )}
         >
             <TextInputField
-                label="Display Name"
-                required
+                label={<span style={{ color: TEXT_BASE }} >Display Name *</span>}
                 placeholder="Enter game name"
                 onChange={(e: ChangeEvent<HTMLInputElement>) => handleInputChange(DISPLAY_NAME, e.target.value)}
             />
             <TextInputField
-                label="Game Path"
-                required
+                label={<span style={{ color: TEXT_BASE }} >Game Path</span>}
                 placeholder="Enter game path"
                 onChange={(e: ChangeEvent<HTMLInputElement>) => handleInputChange(GAME_PATH, e.target.value)}
             />
             <TextInputField
-                label="Cover Image Path"
+                label={<span style={{ color: TEXT_BASE }} >Cover Image Path</span>}
                 placeholder="Enter cover image path"
                 onChange={(e: ChangeEvent<HTMLInputElement>) => handleInputChange(COVER_IMAGE, e.target.value)}
             />
             <TextInputField
-                label="Genre"
+                label={<span style={{ color: TEXT_BASE }} >Genre</span>}
                 placeholder="Enter genre"
                 onChange={(e: ChangeEvent<HTMLInputElement>) => handleInputChange(GENRE, e.target.value)}
             />
             <TextInputField
-                label="Developer"
+                label={<span style={{ color: TEXT_BASE }} >Developer</span>}
                 placeholder="Enter developer name"
                 onChange={(e: ChangeEvent<HTMLInputElement>) => handleInputChange(DEVELOPER, e.target.value)}
             />
             <TextInputField
-                label="Description"
+                label={<span style={{ color: TEXT_BASE }} >Description</span>}
                 placeholder="Enter game description"
                 onChange={(e: ChangeEvent<HTMLInputElement>) => handleInputChange(DESCRIPTION, e.target.value)}
             />
